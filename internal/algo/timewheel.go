e52b9adb5af89f36aa929e843336030f51566dd7
Deepakraj15 <deepakraj15112002@gmail.com>
2026-03-13 22:15:31 +0530

    made wheels as singleton

--- internal/algo/timewheel.go
+++ internal/algo/timewheel.go
@@ -1,1 +1,9 @@
 package algo
+
+import (
+	"github.com/Deepakraj15/task-manager/internal/models"
+)
+
+func submitTask(wheel *models.TimeWheel) {
+
+}
--- internal/models/wheel.go
+++ internal/models/wheel.go
@@ -1,10 +1,45 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/Deepakraj15/task-manager/internal/constants"
+)
 
 type TimeWheel struct {
 	interval  time.Duration
 	slots     []*Slot
 	current   int
 	wheelType string
+}
+
+var hoursWheel *TimeWheel
+var minutesWheel *TimeWheel
+var secondsWheel *TimeWheel
+
+func GetHoursWheel() *TimeWheel {
+	if hoursWheel == nil {
+		hoursWheel = &TimeWheel{
+			wheelType: constants.HOURS,
+		}
+	}
+	return hoursWheel
+}
+
+func GetMinutesWheel() *TimeWheel {
+	if minutesWheel == nil {
+		minutesWheel = &TimeWheel{
+			wheelType: constants.MINUTES,
+		}
+	}
+	return minutesWheel
+}
+
+func GetSecondsWheel() *TimeWheel {
+	if secondsWheel == nil {
+		secondsWheel = &TimeWheel{
+			wheelType: constants.SECONDS,
+		}
+	}
+	return secondsWheel
 }
